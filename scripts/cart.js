34acfac46ad19969c589102ac443d7d53465a116
NenJuJutsu <julian10190@yahoo.com>
2025-07-23 16:31:03 -0600

    updated cart file and added cartStorage file

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,15 +1,4 @@
-import { updateCartUI } from './ui/cartUI.js';
-
-export let cart = [];
-
-export function loadCart() {
-  const stored = localStorage.getItem('cart');
-  if (stored) cart = JSON.parse(stored);
-}
-
-export function saveCart() {
-  localStorage.setItem('cart', JSON.stringify(cart));
-}
+export const cart = [];
 
 export function addToCart(item) {
   const existing = cart.find(i => i.name === item.name);
@@ -18,35 +7,24 @@ export function addToCart(item) {
   } else {
     cart.push({ ...item, qty: 1 });
   }
-  updateCartUI();
-  saveCart();
 }
 
 export function incrementItem(index) {
   cart[index].qty++;
-  updateCartUI();
-  saveCart();
 }
 
 export function decrementItem(index) {
   if (cart[index].qty > 1) {
     cart[index].qty--;
   } else {
-    removeFromCart(index);
-    return;
+    cart.splice(index, 1);
   }
-  updateCartUI();
-  saveCart();
 }
 
 export function removeFromCart(index) {
   cart.splice(index, 1);
-  updateCartUI();
-  saveCart();
 }
 
 export function emptyCart() {
-  cart = [];
-  updateCartUI();
-  saveCart();
+  cart.length = 0;
 }
